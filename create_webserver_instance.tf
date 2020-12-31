e03679554ee55f12ed7bdc9abee668b2bc5381ab
grahbroo <graham.brooks@protonmail.com>
2020-12-31 22:52:09 +0000

    Added more universality

--- create_webserver_instance.tf
+++ create_webserver_instance.tf
@@ -12,18 +12,57 @@ provider "aws" {
     region  = "us-east-1"
 }
 
-resource "aws_instance" "guacamole" {
-    ami = "ami-0be2609ba883822ec"
+resource "aws_instance" "webserver" {
+    ami = "ami-0be2609ba883822e"
     instance_type = "t2.micro"
-    key_name = "Virginia"
-    security_groups = [ "Wordpress" ]
+    key_name = "webserver"
+    security_groups = [ "webserver" ]
+}
+
+resource "aws_security_group" "webserver" {
+    name        = "webserver"
+    description = "Allow SSH and HTTP(S) traffic"
+    
+    ingress {
+        description = "HTTP from VPC"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    ingress {
+        description = "HTTPS from VPC"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "SSH from VPC"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = {
+        Name = "webserver"
+    }
 }
 
 resource "aws_eip_association" "eip_assoc" {
-  instance_id   = aws_instance.guacamole.id
-  allocation_id = aws_eip.guacamole.id
+  instance_id   = aws_instance.webserver.id
+  allocation_id = aws_eip.webserver.id
 }
 
-resource "aws_eip" "guacamole" {
+resource "aws_eip" "webserver" {
   vpc = true
 }
